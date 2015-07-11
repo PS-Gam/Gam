665a5f2a80fa101615b07a1ce0bba573b95991fb
Spencer Baugh <sbaugh@catern.com>
2015-07-10 21:13:04 -0400

    make mainloop more modular

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ use std::io::{self,BufRead};
 use std::process;
 
 fn main() {
+    fn get_challenge<'a>() -> (&'a str, &'a str) {
+        ("Are you having fun? (y/n)", "y")
+    }
+
     let stdin = io::stdin();
     println!("Hello! Welcome to my game!");
     loop {
-        println!("Are you having fun? (y/n)");
+        let (challenge, verify) = get_challenge();
+
+        println!("{}", challenge);
+
         let response = stdin.lock().lines().next().unwrap().unwrap();
-        if response != "y" {
+        if response != verify {
             println!("WELL SCREW YOU");
             process::exit(1);
         }
