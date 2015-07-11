a96ee946a21161bf3ec3fc78f5661a9c16d62baf
Spencer Baugh <sbaugh@catern.com>
2015-07-10 21:04:32 -0400

    add basic challenge-response loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self,BufRead};
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    println!("Hello! Welcome to my game!");
+    loop {
+        println!("Are you having fun? (y/n)");
+        let response = stdin.lock().lines().next().unwrap().unwrap();
+        if response != "y" {
+            println!("WELL SCREW YOU");
+            process::exit(1);
+        }
+    }
 }
+
